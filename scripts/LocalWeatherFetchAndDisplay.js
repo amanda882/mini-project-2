5c5da0aef0ee72244aada3855e656936fe0f869b
Jadyn-Jenkins <Noblejadynjenkins@gmail.com>
2022-12-02 16:55:29 -0500

    code condensed and working together. FixLocalWeather....js. deleted duplicate js files. altered weatherPage.html to help js run

--- scripts/LocalWeatherFetchAndDisplay.js
+++ scripts/LocalWeatherFetchAndDisplay.js
@@ -1,55 +1,51 @@
-//declare html elements
-
-//Check error
-
-// Our Server http://localhost:3004/cities
-
+// Our Server City Data Server => http://localhost:3004/cities
 fetch("http://localhost:3004/cities")
   .then((results) => results.json())
   .then((data) => {
-    console.log(data);
-
     let cityList = document.getElementById("selectDrop");
     let cityArrayLength = data.length;
 
-    console.log(`number of cities is ${cityArrayLength}`);
-
+    //console.log(`number of cities is ${cityArrayLength}`);
     for (i = 0; i < cityArrayLength; i++) {
       let theOption = new Option(data[i].name);
-      // console.log(theOption);
       cityList.appendChild(theOption);
     }
 
     cityList.onchange = () => {
-        
-    }
+      let cityOption = cityList.value;
+      let selectedCity = data.find((arrItem) => arrItem.name == cityOption);
+
+      getWeather(selectedCity.latitude, selectedCity.longitude);
+    };
   });
 
-//select
-
-function getWeather() {
-  fetch("https://api.weather.gov/points/32.6791,-97.4641")
+function getWeather(lat, long) {
+  fetch(`https://api.weather.gov/points/${lat},${long}`)
     .then((response) => {
-      console.log("resolved", response);
+      //console.log("resolved", response);
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      //console.log("resolved here", data);
       fetch(data.properties.forecast)
         .then((results) => {
+          //console.log("resolved again", results);
           return results.json();
         })
         .then((forecastData) => {
-          console.log(forecastData);
+          //console.log(forecastData.properties.periods);
+          createForecastTable(forecastData.properties.periods);
         });
     })
     .catch((err) => {
-      console.log("rejected", err);
+      console.error("rejected", err);
     });
 }
 
 function createForecastTable(data) {
   //Create Table
+  let output = document.querySelector("output");
+
   let table = document.createElement("table");
 
   let tableHeaders = [
@@ -69,6 +65,8 @@ function createForecastTable(data) {
   }
 
   //Create row and cells with content
+
+  //console.log("cells creation start", data);
   for (let i = 0; i < data.length; i++) {
     let row = document.createElement("tr");
 
