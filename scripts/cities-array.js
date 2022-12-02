1d1686c0fe078b6734ce283ee5aa7caeb3f7d344
Eliajah Shepard <Eshepard@pgh.yearup.org>
2022-12-02 10:15:42 -0500

    add more cities to array

--- scripts/cities-array.js
+++ scripts/cities-array.js
@@ -16,14 +16,54 @@
 
 
 let cities = [
-    {
-       "name":"Colombus, Ohio ",
-       "latitude":39.962524,
-       "longitude":-82.99796
-    },
-    {
-       "name":"San Diego, California ",
-       "latitude":32.715736,
-       "longitude":-117.161087
-    }
- ]
+   {
+      "name": "Colombus, Ohio ",
+      "latitude": 39.962524,
+      "longitude": -82.99796
+   },
+   {
+      "name": "San Diego, California ",
+      "latitude": 32.715736,
+      "longitude": -117.161087
+   },
+   {
+      "name": "Savannah, Georgia ",
+      "latitude": 32.076176,
+      "longitude": -81.088371
+   },
+   {
+      "name": "Boise, Idaho ",
+      "latitude": 43.618881,
+      "longitude": -116.215019
+   },
+   {
+      "name": "Newark, Delaware ",
+      "latitude": 39.683723,
+      "longitude": -75.749657
+   },
+
+   {
+      "name": "Tampa, Florida ",
+      "latitude": 27.9506,
+      "longitude": -82.4572
+   },
+
+   {
+      "name": "Knoxville, Tennessee ",
+      "latitude": 35.9606,
+      "longitude": -83.926453
+   },
+
+   {
+      "name": "Jacksonville,Florida ",
+      "latitude": 30.3322,
+      "longitude": 81.6557
+   },
+
+   {
+      "name": "Devner, Colorado ",
+      "latitude": 39.7392,
+      "longitude": 104.9903
+   }
+
+]
